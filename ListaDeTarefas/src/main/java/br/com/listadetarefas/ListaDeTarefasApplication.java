787ff5e096de2096b46889011545aa61637eff71
Thaynan Andrey <thaynan.nunes@ccc.ufcg.edu.br>
2017-01-25 19:13:47 -0200

    Inicializando o projeto Lista de Tarefas

---
+++ ListaDeTarefas/src/main/java/br/com/listadetarefas/ListaDeTarefasApplication.java
@@ -0,0 +1,12 @@
+package br.com.listadetarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaDeTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaDeTarefasApplication.class, args);
+	}
+}
